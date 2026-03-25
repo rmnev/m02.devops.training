a04993a74ae9fdd453b1f6b22e7ce5aa2d4075a7
rmnev <roman.mnev@rt-rk.com>
2026-03-25 10:13:20 +0100

    Update factorial.py

--- 1-tdd/factorial.py
+++ 1-tdd/factorial.py
@@ -1,4 +1,5 @@
 # define your solution
 def factorial(n):
+    if n < 0:
+        raise ValueError("Factorial is not defined for negative numbers")
     return 1 if n <= 1 else n * factorial(n - 1)
-
