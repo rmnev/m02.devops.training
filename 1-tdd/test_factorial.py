30038acbf2b229d482b640aeffca32e47f486922
rmnev <roman.mnev@rt-rk.com>
2026-03-25 10:15:34 +0100

    Update test_factorial.py

--- 1-tdd/test_factorial.py
+++ 1-tdd/test_factorial.py
@@ -3,7 +3,7 @@ from factorial import factorial
 
 class TestFactorial(unittest.TestCase):
     def test_factorial_of_0(self):
-        self.assertEqual(factorial(0), 1)
+        self.assertEqual(factorial(0), 18)
 
     def test_factorial_of_1(self):
         self.assertEqual(factorial(1), 1)
