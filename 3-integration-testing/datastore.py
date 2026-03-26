4b1baa26f0d96213f5e06cf6c5f879b806d6a384
rmnev <roman.mnev@rt-rk.com>
2026-03-26 12:05:51 +0100

    Update datastore.py

--- 3-integration-testing/datastore.py
+++ 3-integration-testing/datastore.py
@@ -1,20 +1,2 @@
+# datastore.py
 database = {}
-
-def store_value(key, value):
-    """Store a value in the dictionary under the given key."""
-    database[key] = value
-
-def get_value(key):
-    """Retrieve a value by key. Return None if the key does not exist."""
-    return database.get(key, None)
-
-def delete_value(key):
-    """Delete a value by key. Return True if deleted, False if key was not found."""
-    if key in database:
-        del database[key]
-        return True
-    return False
-
-def list_keys():
-    """Return a list of all keys in the dictionary."""
-    return list(database.keys())
