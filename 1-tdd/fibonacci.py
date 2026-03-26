c5af61cdcef32f0c10a7f31b6cd6aa20907eaf56
rmnev <roman.mnev@rt-rk.com>
2026-03-26 10:16:10 +0100

    Update fibonacci.py

--- 1-tdd/fibonacci.py
+++ 1-tdd/fibonacci.py
@@ -1,4 +1,12 @@
 # define your solution
 def fibonacci(n):
-    pass
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
 
+    a, b = 0, 1
+    for _ in range(2, n + 1):
+        a, b = b, a + b
+
+    return b
